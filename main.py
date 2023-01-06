8d93db9d2a3cd1e6fa5027e202a616f2a9aea586
Shriram Hemant Salunke <74704080+Shriram-Salunke-045@users.noreply.github.com>
2023-01-06 15:25:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-# pip install pandas
-# pip install pandas-profiling
+# To install the libraries
+pip install pandas
+pip install pandas-profiling
+pip install numpy
 
 import pandas as pd
 from pandas_profiling import ProfileReport
 df = pd.read_csv('police_department_data.csv')
 print(df)
 
+# to generate a report
 profile  = ProfileReport(df)
 profile.to_file(output_file="police_department_data.csv")
